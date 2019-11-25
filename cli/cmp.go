66853a19274ab9ca381630923a44fb15157162ec
Klaus Post <klauspost@gmail.com>
2019-11-25 18:23:53 +0100

    Added benchmark comparisons `cmp`

---
+++ cli/cmp.go
@@ -0,0 +1,145 @@
+/*
+ * Warp (C) 2019- MinIO, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cli
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/fatih/color"
+	"github.com/klauspost/compress/zstd"
+	"github.com/minio/cli"
+	"github.com/minio/mc/pkg/console"
+	"github.com/minio/mc/pkg/probe"
+	"github.com/minio/warp/pkg/bench"
+)
+
+var cmpFlags = []cli.Flag{}
+
+var cmpCmd = cli.Command{
+	Name:   "cmp",
+	Usage:  "compare existing benchmark data",
+	Action: mainCmp,
+	Before: setGlobalsFromContext,
+	Flags:  combineFlags(globalFlags, analyzeFlags, cmpFlags),
+	CustomHelpTemplate: `NAME:
+  {{.HelpName}} - {{.Usage}}
+
+USAGE:
+  {{.HelpName}} [FLAGS] before-benchmark-data-file after-benchmark-data-file
+
+FLAGS:
+  {{range .VisibleFlags}}{{.}}
+  {{end}}
+
+EXAMPLES:
+...
+ `,
+}
+
+// mainAnalyze is the entry point for analyze command.
+func mainCmp(ctx *cli.Context) error {
+	checkAnalyze(ctx)
+	checkCmp(ctx)
+	args := ctx.Args()
+	var zstdDec, _ = zstd.NewReader(nil)
+	defer zstdDec.Close()
+	readOps := func(s string) bench.Operations {
+		f, err := os.Open(s)
+		fatalIf(probe.NewError(err), "Unable to open input file")
+		defer f.Close()
+		err = zstdDec.Reset(f)
+		fatalIf(probe.NewError(err), "Unable to decompress input")
+		b, err := ioutil.ReadAll(zstdDec)
+		fatalIf(probe.NewError(err), "Unable to read input")
+		ops, err := bench.OperationsFromCSV(bytes.NewBuffer(b))
+		fatalIf(probe.NewError(err), "Unable to parse input")
+		return ops
+	}
+	printCompare(ctx, readOps(args[0]), readOps(args[1]))
+	return nil
+}
+
+func printCompare(ctx *cli.Context, before, after bench.Operations) {
+	var wrSegs io.Writer
+
+	if fn := ctx.String("compare.out"); fn != "" {
+		if fn == "-" {
+			wrSegs = os.Stdout
+		} else {
+			f, err := os.Create(fn)
+			fatalIf(probe.NewError(err), "Unable to create create analysis output")
+			defer console.Println("Aggregated data saved to", fn)
+			defer f.Close()
+			wrSegs = f
+		}
+	}
+	_ = wrSegs
+	timeDur := func(ops bench.Operations) time.Duration {
+		start, end := ops.ActiveTimeRange()
+		return end.Sub(start).Round(time.Second)
+	}
+
+	for typ, _ := range before.ByOp() {
+		if wantOp := ctx.String("analyze.op"); wantOp != "" {
+			if wantOp != typ {
+				continue
+			}
+		}
+		before := before.FilterByOp(typ)
+		after := after.FilterByOp(typ)
+		console.Println("-------------------")
+		cmp, err := bench.Compare(before, after, analysisDur(ctx))
+		if err != nil {
+			console.Println(err)
+			continue
+		}
+
+		console.SetColor("Print", color.New(color.FgHiWhite))
+		console.Println("Operation:", typ)
+		console.SetColor("Print", color.New(color.FgWhite))
+		if len(before) != len(after) {
+			console.Println("Operations:", len(before), "->", len(after))
+		}
+		if before.Threads() != after.Threads() {
+			console.Println("Concurrency:", before.Threads(), "->", after.Threads())
+		}
+		if before.FirstObjPerOp() != after.FirstObjPerOp() {
+			console.Println("Objects per operation:", before.FirstObjPerOp(), "->", after.FirstObjPerOp())
+		}
+		if timeDur(before) != timeDur(after) {
+			console.Println("Duration:", timeDur(before), "->", timeDur(after))
+		}
+		console.Println("* Average:", cmp.Average)
+		if cmp.TTFB != nil {
+			console.Println("* First Byte:", cmp.TTFB)
+		}
+		console.SetColor("Print", color.New(color.FgWhite))
+		console.Println("* Fastest:", cmp.Fastest)
+		console.Println("* 50% Median:", cmp.Median)
+		console.Println("* Slowest:", cmp.Slowest)
+	}
+}
+
+func checkCmp(ctx *cli.Context) {
+	if ctx.NArg() != 2 {
+		console.Fatal("Two data sources must be supplied")
+	}
+}
---
+++ pkg/bench/compare.go
@@ -0,0 +1,165 @@
+package bench
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"time"
+)
+
+// Comparison is a comparison between two benchmarks.
+type Comparison struct {
+	Op string
+
+	TTFB *TTFBCmp
+
+	Average CmpSegment
+	Fastest CmpSegment
+	Median  CmpSegment
+	Slowest CmpSegment
+}
+
+// CmpSegment is s comparisons between two segments.
+type CmpSegment struct {
+	Before, After    *Segment
+	ThroughputPerSec float64
+	ObjPerSec        float64
+	OpsEndedPerSec   float64
+}
+
+// Compare sets c to a comparison between before and after.
+func (c *CmpSegment) Compare(before, after Segment) {
+	c.Before = &before
+	c.After = &after
+	mbB, opsB, objsB := before.SpeedPerSec()
+	mbA, opsA, objsA := after.SpeedPerSec()
+	c.ObjPerSec = 100 * (objsA - objsB) / objsB
+	c.OpsEndedPerSec = 100 * (opsA - opsB) / opsB
+	if mbB > 0 {
+		c.ThroughputPerSec = 100 * (mbA - mbB) / mbB
+	}
+}
+
+// String returns a string representation of the segment comparison.
+func (c CmpSegment) String() string {
+	speed := ""
+	if c.ThroughputPerSec > 0 {
+		speed = fmt.Sprintf("%s%.02f%% throughput, ", plusPositiveF(c.ThroughputPerSec), c.ThroughputPerSec)
+	}
+	return fmt.Sprintf("%s%s%.02f%% obj/s",
+		speed, plusPositiveF(c.ObjPerSec), c.ObjPerSec)
+}
+
+func plusPositiveF(f float64) string {
+	switch {
+	case f > 0 && !math.IsInf(f, 1):
+		return "+"
+	default:
+		return ""
+	}
+}
+
+// TTFBCmp is a comparison between two TTFB runs.
+type TTFBCmp struct {
+	TTFB
+	Before, After TTFB
+}
+
+// Compare will set t to the difference between before and after.
+func (t TTFB) Compare(after TTFB) *TTFBCmp {
+	if t.Average == 0 {
+		return nil
+	}
+	return &TTFBCmp{
+		TTFB: TTFB{
+			Average: after.Average - t.Average,
+			Worst:   after.Worst - t.Worst,
+			Best:    after.Best - t.Best,
+			Median:  after.Median - t.Median,
+		},
+		Before: t,
+		After:  after,
+	}
+}
+
+// String returns a human readable representation of the TTFB comparison.
+func (t *TTFBCmp) String() string {
+	if t == nil {
+		return ""
+	}
+	return fmt.Sprintf("Average: %s%v (%s%.f%%), Median: %s%v (%s%.f%%), Best: %s%v (%s%.f%%), Worst: %s%v (%s%.f%%)",
+		plusPositiveD(t.Average),
+		t.Average,
+		plusPositiveD(t.Average),
+		100*(float64(t.After.Average)-float64(t.Before.Average))/float64(t.Before.Average),
+		plusPositiveD(t.Median),
+		t.Median,
+		plusPositiveD(t.Median),
+		100*(float64(t.After.Median)-float64(t.Before.Median))/float64(t.Before.Median),
+		plusPositiveD(t.Best),
+		t.Best,
+		plusPositiveD(t.Best),
+		100*(float64(t.After.Best)-float64(t.Before.Best))/float64(t.Before.Best),
+		plusPositiveD(t.Worst),
+		t.Worst,
+		plusPositiveD(t.Worst),
+		100*(float64(t.After.Worst)-float64(t.Before.Worst))/float64(t.Before.Worst),
+	)
+}
+
+func plusPositiveD(d time.Duration) string {
+	switch {
+	case d > 0:
+		return "+"
+	default:
+		return ""
+	}
+}
+
+// Compare compares operations of a single operation type.
+func Compare(before, after Operations, analysis time.Duration) (*Comparison, error) {
+	var res Comparison
+	if before.FirstOpType() != after.FirstOpType() {
+		return nil, fmt.Errorf("different operation types. before: %v, after %d", before.FirstOpType(), after.FirstOpType())
+	}
+	if len(before.Errors()) > 0 || len(after.Errors()) > 0 {
+		return nil, fmt.Errorf("errors recorded in benchmark run. before: %v, after %d", len(before.Errors()), len(after.Errors()))
+	}
+	res.Op = before.FirstOpType()
+
+	segment := func(ops Operations) (Segments, error) {
+		segs := ops.Segment(SegmentOptions{
+			From:           time.Time{},
+			PerSegDuration: analysis,
+		})
+		if len(segs) <= 1 {
+			return nil, errors.New("too few samples")
+		}
+		totals, _ := ops.Total()
+		if totals.TotalBytes > 0 {
+			segs.SortByThroughput()
+		} else {
+			segs.SortByObjsPerSec()
+		}
+		return segs, nil
+	}
+	bs, err := segment(before)
+	if err != nil {
+		return nil, fmt.Errorf("segmenting before: %w", err)
+	}
+	as, err := segment(after)
+	if err != nil {
+		return nil, fmt.Errorf("segmenting after: %w", err)
+	}
+
+	res.Median.Compare(bs.Median(0.5), as.Median(0.5))
+	res.Slowest.Compare(bs.Median(0.0), as.Median(0.0))
+	res.Fastest.Compare(bs.Median(1), as.Median(1))
+
+	beforeTotals, beforeTTFB := before.Total()
+	afterTotals, afterTTFB := after.Total()
+
+	res.Average.Compare(beforeTotals, afterTotals)
+	res.TTFB = beforeTTFB.Compare(afterTTFB)
+	return &res, nil
+}
