397185a05a33f239c5b8096b5bbb2adfe30deb62
Klaus Post <klauspost@gmail.com>
2020-11-24 12:08:37 +0100

    Respect skipdur even with errors.

--- pkg/aggregate/aggregate.go
+++ pkg/aggregate/aggregate.go
@@ -103,6 +103,11 @@ func Aggregate(o bench.Operations, opts Options) Aggregated {
 			o = o.FilterInsideRange(start, end)
 			ops = o
 		} else {
+			if opts.SkipDur > 0 {
+				start, end := o.TimeRange()
+				start.Add(opts.SkipDur)
+				o = o.FilterInsideRange(start, end)
+			}
 			ops = o.FilterSuccessful()
 		}
 
