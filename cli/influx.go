345afa69348df92e24d63347af1eb068c9a9309d
Jason Cwik <jcwik@ddn.com>
2024-10-29 18:54:18 -0500

    Changed stat aggregation for influx
    Under very high concurrency, logging every transaction to Influx can
    overwhelm the Influx server. This changes the client code to aggregate
    data and only report one data point per second. Thus, when there are
    hundreds of clients you're only logging hundreds of data points per
    second instead of potentially millions.

--- cli/influx.go
+++ cli/influx.go
@@ -28,6 +28,7 @@ import (
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	influxapi "github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/influxdata/influxdb-client-go/v2/api/http"
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/minio/cli"
@@ -84,69 +85,113 @@ func newInfluxDB(ctx *cli.Context, wg *sync.WaitGroup) chan<- bench.Operation {
 		return false
 	})
 
-	noAggregate := ctx.Bool("no-aggregate")
-
 	ch := make(chan bench.Operation, 10000)
 	wg.Add(1)
 	go func() {
-		defer func() {
-			writeAPI.Flush()
-			wg.Done()
-		}()
+
 		hosts := make(map[string]map[string]aggregatedStats, 100)
 		totalOp := make(map[string]aggregatedStats, 5)
-		for op := range ch {
-			host := hosts[op.Endpoint]
-			var hostStats aggregatedStats
-			if host == nil {
-				host = make(map[string]aggregatedStats, 5)
-				hosts[op.Endpoint] = host
-			} else {
-				hostStats = host[op.OpType]
-			}
-			total := totalOp[op.OpType]
-			hostStats.add(op)
-			total.add(op)
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		defer wg.Done()
 
-			// Store
-			totalOp[op.OpType] = total
-			host[op.OpType] = hostStats
-
-			// Send
-			pTot := total.point(op, noAggregate)
-			pHost := hostStats.point(op, noAggregate)
-
-			for key, tag := range tags {
-				pTot.AddTag(key, tag)
-				pHost.AddTag(key, tag)
-			}
-			pHost.AddTag("endpoint", op.Endpoint)
-			writeAPI.WritePoint(pHost)
-			writeAPI.WritePoint(pTot)
-		}
-		// Send summaries
-		for host, ops := range hosts {
-			for op, stats := range ops {
-				p := stats.summary(op)
-
-				for key, tag := range tags {
-					p.AddTag(key, tag)
+		for {
+			select {
+			case op, ok := <-ch:
+				if !ok {
+					// Channel closed, flush remaining data
+					flushData(hosts, totalOp, tags, writeAPI)
+					sendSummaries(hosts, totalOp, tags, writeAPI)
+					writeAPI.Flush()
+					return
 				}
-				p.AddTag("endpoint", host)
-				writeAPI.WritePoint(p)
-			}
-		}
-		for op, stats := range totalOp {
-			p := stats.summary(op)
 
-			for key, tag := range tags {
-				p.AddTag(key, tag)
+				host := hosts[op.Endpoint]
+				var hostStats aggregatedStats
+				if host == nil {
+					host = make(map[string]aggregatedStats, 5)
+					hosts[op.Endpoint] = host
+				} else {
+					hostStats = host[op.OpType]
+				}
+				total := totalOp[op.OpType]
+				hostStats.add(op)
+				total.add(op)
+
+				// Store
+				totalOp[op.OpType] = total
+				host[op.OpType] = hostStats
+
+			case <-ticker.C:
+				flushData(hosts, totalOp, tags, writeAPI)
 			}
-			p.AddTag("endpoint", "")
-			writeAPI.WritePoint(p)
 		}
 	}()
 	return ch
+}
+
+func sendSummaries(hosts map[string]map[string]aggregatedStats, totalOp map[string]aggregatedStats,
+	tags map[string]string, writeAPI influxapi.WriteAPI) {
+
+	// Send summaries for each host
+	for host, ops := range hosts {
+		for op, stats := range ops {
+			p := stats.summary(op)
+			for key, tag := range tags {
+				p.AddTag(key, tag)
+			}
+			p.AddTag("endpoint", host)
+			writeAPI.WritePoint(p)
+		}
+	}
+
+	// Send summaries for total operations
+	for op, stats := range totalOp {
+		p := stats.summary(op)
+		for key, tag := range tags {
+			p.AddTag(key, tag)
+		}
+		p.AddTag("endpoint", "")
+		writeAPI.WritePoint(p)
+	}
+}
+
+func flushData(hosts map[string]map[string]aggregatedStats, totalOp map[string]aggregatedStats, tags map[string]string, writeAPI influxapi.WriteAPI) {
+	for endpoint, hostData := range hosts {
+		for opType, hostStats := range hostData {
+			if hostStats.incr.ops == 0 {
+				// Don't write empty results.
+				continue
+			}
+			pHost := hostStats.point(bench.Operation{OpType: opType})
+			for key, tag := range tags {
+				pHost.AddTag(key, tag)
+			}
+			pHost.AddTag("endpoint", endpoint)
+			writeAPI.WritePoint(pHost)
+
+			// Reset only incremental stats
+			hostStats.incr = aggregatedStats{}.incr
+			hosts[endpoint][opType] = hostStats
+		}
+	}
+
+	for opType, totalStats := range totalOp {
+		if totalStats.incr.ops == 0 {
+			// Don't write empty results.
+			continue
+		}
+		pTot := totalStats.point(bench.Operation{OpType: opType})
+		for key, tag := range tags {
+			pTot.AddTag(key, tag)
+		}
+		pTot.AddTag("endpoint", "")
+		writeAPI.WritePoint(pTot)
+
+		// Reset only incremental stats
+		totalStats.incr = aggregatedStats{}.incr
+		totalOp[opType] = totalStats
+	}
 }
 
 func parseInfluxURL(ctx *cli.Context) (*url.URL, error) {
@@ -185,102 +230,108 @@ func parseInfluxURL(ctx *cli.Context) (*url.URL, error) {
 }
 
 type aggregatedStats struct {
-	bytes   int64
-	objects int
-	ops     int
-	errors  int
+	// Incremental stats (reset every second)
+	incr struct {
+		bytes   int64
+		objects int
+		ops     int
+		errors  int
+		reqDur  time.Duration
+		ttfb    time.Duration
+	}
 
-	// requests
-	reqDur time.Duration
-	reqMin time.Duration
-	reqMax time.Duration
+	// Total stats (accumulated over the entire run)
+	total struct {
+		bytes   int64
+		objects int
+		ops     int
+		errors  int
+		reqDur  time.Duration
+		ttfb    time.Duration
+	}
 
-	// time to first byte
-	ttfb    time.Duration
+	// These remain as before, tracking min/max for the entire run
+	reqMin  time.Duration
+	reqMax  time.Duration
 	ttfbMin time.Duration
 	ttfbMax time.Duration
 }
 
 func (a *aggregatedStats) add(o bench.Operation) {
-	a.ops++
+	// Update incremental stats
+	a.incr.ops++
+	a.incr.bytes += o.Size
+	a.incr.objects += o.ObjPerOp
+
+	// Update total stats
+	a.total.ops++
+	a.total.bytes += o.Size
+	a.total.objects += o.ObjPerOp
+
 	if o.Err != "" {
-		// Do not add more
-		a.errors++
+		a.incr.errors++
+		a.total.errors++
 		return
 	}
 
-	a.bytes += o.Size
-	a.objects += o.ObjPerOp
-
 	dur := o.End.Sub(o.Start)
-	a.reqDur += dur
+	a.incr.reqDur += dur
+	a.total.reqDur += dur
+
 	if dur > a.reqMax {
 		a.reqMax = dur
 	}
 	if a.reqMin == 0 || dur < a.reqMin {
 		a.reqMin = dur
 	}
+
 	if o.FirstByte != nil {
 		ttfb := o.FirstByte.Sub(o.Start)
-		a.ttfb += ttfb
+		a.incr.ttfb += ttfb
+		a.total.ttfb += ttfb
+
 		if ttfb > a.ttfbMax {
 			a.ttfbMax = ttfb
 		}
-		if a.ttfbMin == 0 || dur < a.ttfbMin {
-			a.ttfbMin = dur
+		if a.ttfbMin == 0 || ttfb < a.ttfbMin {
+			a.ttfbMin = ttfb
 		}
 	}
 }
 
-func (a aggregatedStats) point(op bench.Operation, noAggregate bool) *write.Point {
+func (a aggregatedStats) point(op bench.Operation) *write.Point {
 	p := influxdb2.NewPointWithMeasurement("warp")
 	p.AddTag("op", op.OpType)
 
-	if noAggregate {
-		p.AddField("requests", 1)
-		p.AddField("objects", op.ObjPerOp)
-		p.AddField("bytes_total", op.Size)
-		p.AddField("request_total_secs", float64(op.End.Sub(op.Start))/float64(time.Second))
-		if op.FirstByte != nil {
-			p.AddField("request_ttfb_avg_secs", float64(op.FirstByte.Sub(op.Start))/float64(time.Second))
-		}
-		errs := 0
-		if op.Err != "" {
-			errs = 1
-		}
-		p.AddField("errors", errs)
-		p.SetTime(op.End)
-	} else {
-		p.AddField("requests", a.ops)
-		p.AddField("objects", a.objects)
-		p.AddField("bytes_total", a.bytes)
-		p.AddField("errors", a.errors)
-		p.AddField("request_total_secs", float64(a.reqDur)/float64(time.Second))
-		if a.ttfb > 0 {
-			p.AddField("request_ttfb_total_secs", float64(a.ttfb)/float64(time.Second))
-		}
+	p.AddField("requests", a.incr.ops)
+	p.AddField("objects", a.incr.objects)
+	p.AddField("bytes_total", a.incr.bytes)
+	p.AddField("errors", a.incr.errors)
+	// Average
+	p.AddField("request_total_secs", (float64(a.incr.reqDur)/float64(time.Second))/float64(a.incr.ops))
+	if a.incr.ttfb > 0 {
+		// Average
+		p.AddField("request_ttfb_total_secs", (float64(a.incr.ttfb)/float64(time.Second))/float64(a.incr.ops))
 	}
+
 	return p
 }
 
 func (a aggregatedStats) summary(opType string) *write.Point {
 	p := influxdb2.NewPointWithMeasurement("warp_run_summary")
 	p.AddTag("op", opType)
-	p.AddField("requests", a.ops)
-	p.AddField("objects", a.objects)
-	p.AddField("bytes_total", a.bytes)
-	p.AddField("errors", a.errors)
-	p.AddField("request_total_secs", float64(a.reqDur)/float64(time.Second))
-	if a.ops-a.errors > 0 {
-		p.AddField("request_avg_secs", float64(a.reqDur)/float64(time.Second)/float64(a.ops-a.errors))
+	p.AddField("requests", a.total.ops)
+	p.AddField("objects", a.total.objects)
+	p.AddField("bytes_total", a.total.bytes)
+	p.AddField("errors", a.total.errors)
+	p.AddField("request_total_secs", float64(a.total.reqDur)/float64(time.Second))
+	if a.total.ops-a.total.errors > 0 {
+		p.AddField("request_avg_secs", float64(a.total.reqDur)/float64(time.Second)/float64(a.total.ops-a.total.errors))
 	}
 	p.AddField("request_min_secs", float64(a.reqMin)/float64(time.Second))
 	p.AddField("request_max_secs", float64(a.reqMax)/float64(time.Second))
-	if a.ttfb > 0 {
-		p.AddField("request_ttfb_total_secs", float64(a.ttfb)/float64(time.Second))
-		if a.ops-a.errors > 0 {
-			p.AddField("request_ttfb_avg_secs", float64(a.ttfb)/float64(time.Second)/float64(a.ops-a.errors))
-		}
+	if a.total.ttfb > 0 {
+		p.AddField("request_ttfb_avg_secs", float64(a.total.ttfb)/float64(time.Second)/float64(a.total.ops-a.total.errors))
 		p.AddField("request_ttfb_min_secs", float64(a.ttfbMin)/float64(time.Second))
 		p.AddField("request_ttfb_max_secs", float64(a.ttfbMax)/float64(time.Second))
 	}
