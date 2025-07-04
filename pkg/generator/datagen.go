0f99726860ddbceaadcd263b749ccd6101148d3e
Russ-Fellows <russ@evaluatorgroup.com>
2025-07-04 15:03:11 -0600

    Update datagen.go

--- pkg/generator/datagen.go
+++ pkg/generator/datagen.go
@@ -1,5 +1,5 @@
 /*
- * Datagen (C) 2025 Signal65 / Futurum Group LLC.
+ * This file (C) 2025 Signal65 / Futurum Group LLC.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
