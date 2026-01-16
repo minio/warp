692bf651b867d5c548c7384eb6d13a14af7c942a
M Alvee <alvee.developer@gmail.com>
2026-01-16 12:22:48 +0600

    license

--- pkg/bench/iceberg_write.go
+++ pkg/bench/iceberg_write.go
@@ -1,5 +1,5 @@
 /*
- * Warp (C) 2019-2024 MinIO, Inc.
+ * Warp (C) 2019-2026 MinIO, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
